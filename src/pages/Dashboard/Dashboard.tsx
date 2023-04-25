9c4c4168920f88a82f2d68ca5a6de36486409573
jBarrios26 <jorge.barrios@galileo.edu>
2023-04-24 22:32:28 -0600

    Agregar layout de dashboard responsive

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { ReactElement, ReactNode } from 'react';
+import { classNames } from '../../util/classnames';
 
-const Dashboard: React.FC = () => {
-  return <div>Dashboard</div>;
-};
+function Dashboard() {
+  return (
+    <div className="row-auto grid gap-2 p-2 first:mt-0 md:grid-cols-6 md:p-8 ">
+      <div className="col-span-1 flex flex-col gap-2 md:col-span-6 lg:col-span-4">
+        <Card className="h-36">Hola mundo 1</Card>
+        <Card className=" h-80">Hola mundo 2</Card>
+      </div>
+      <div className="col-span-1 flex h-full w-full flex-col gap-2 md:col-span-6 lg:col-span-2">
+        <Card className="lg:h-1/2">Hola mundo 3</Card>
+        <Card className="lg:h-1/2">Hola mundo 4</Card>
+      </div>
+      <Card className=" col-span-1 md:col-span-6 lg:col-span-2">
+        Hola mundo 5
+      </Card>
+    </div>
+  );
+}
+
+function Card(props: { children: ReactNode; className?: string }) {
+  return (
+    <div
+      className={classNames(
+        'w-full rounded-lg bg-dark-blue-custom py-2 px-2',
+        `${props.className ?? ''}`
+      )}
+    >
+      {props.children}
+    </div>
+  );
+}
 
 export default Dashboard;
